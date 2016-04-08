bf6e171f40ab818006ba214945369fb4e9aa7ef9
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2016-04-09 02:14:26 +1000

    Cleaned up RepresentationTest

--- testing/src/main/java/me/stockwells/util/testing/RepresentationTest.java
+++ testing/src/main/java/me/stockwells/util/testing/RepresentationTest.java
@@ -5,27 +5,35 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Throwables;
 import io.dropwizard.jackson.Jackson;
+import io.dropwizard.testing.FixtureHelpers;
 import nl.jqno.equalsverifier.EqualsVerifier;
+import org.apache.commons.io.FilenameUtils;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Collection;
-import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONAs;
-
 
 public abstract class RepresentationTest<T> {
 	private final ObjectMapper mapper = objectMapper();
 
 	protected abstract TypeReference<T> type();
-	protected abstract Collection<Fixture> fixtures();
-
+	protected abstract Collection<Fixture<T>> fixtures();
 
 	@Test
 	public void equalsContract() {
@@ -35,18 +43,25 @@ public abstract class RepresentationTest<T> {
 	@Test
 	public void deserialisation() throws IOException {
 		fixtures().stream()
-			.filter(f -> f.getDirection() != Direction.SERIALISE)
+			.filter(f -> f.getSerialisationMode() != SerialisationMode.SERIALISE)
 			.forEach(this::assertDeserialisation);
 	}
 
 	@Test
 	public void serialisation() throws JsonProcessingException {
 		fixtures().stream()
-			.filter(f -> f.getDirection() != Direction.DESERIALISE)
+			.filter(f -> f.getSerialisationMode() != SerialisationMode.DESERIALISE)
 			.forEach(this::assertSerialisation);
 	}
 
-	public void assertDeserialisation(Fixture f) {
+	@Test
+	public void validation() {
+		fixtures().stream()
+			.filter(f -> f.getValidationMode() != ValidationMode.NONE)
+			.forEach(this::assertValidation);
+	}
+
+	protected void assertDeserialisation(Fixture<T> f) {
 		try {
 			assertThat(
 				f.getFixture(),
@@ -58,7 +73,7 @@ public abstract class RepresentationTest<T> {
 		}
 	}
 
-	public void assertSerialisation(Fixture f) {
+	protected void assertSerialisation(Fixture<T> f) {
 		try {
 			assertThat(
 				f.getFixture(),
@@ -70,6 +85,23 @@ public abstract class RepresentationTest<T> {
 		}
 	}
 
+	protected void assertValidation(Fixture<T> f) {
+		try {
+			Set<ConstraintViolation<T>> violations = validator().validate(deserialise(fixture(f.getFixture())));
+			if(f.getValidationMode() == ValidationMode.INVALIDATE) {
+				assertThat("Expected violations, none found", violations, not(empty()));
+			} else {
+				assertThat(violationsMessage(violations), violations, empty());
+			}
+		} catch(IOException e) {
+			Throwables.propagate(e);
+		}
+	}
+
+	protected String violationsMessage(Collection<ConstraintViolation<T>> violations) {
+		return String.join("\n", violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.toSet()));
+	}
+
 	protected String serialise(T representation) throws JsonProcessingException {
 		return mapper.writeValueAsString(representation);
 	}
@@ -78,12 +110,20 @@ public abstract class RepresentationTest<T> {
 		return mapper.readValue(fixture, type());
 	}
 
+	protected String fixture(String fixture) {
+		return FixtureHelpers.fixture(FilenameUtils.concat(fixtureDir(), fixture));
+	}
+
 	@SuppressWarnings("unchecked")
 	protected Class<T> getClassType() {
 		Type t = type().getType();
 		return (t instanceof ParameterizedType)
-			? (Class<T>) ((ParameterizedType)t).getRawType()
+			? (Class<T>) ((ParameterizedType) t).getRawType()
 			: (Class<T>) t;
+	}
+
+	protected String fixtureDir() {
+		return "";
 	}
 
 	protected EqualsVerifier<T> equalsVerifier() {
@@ -94,26 +134,24 @@ public abstract class RepresentationTest<T> {
 		return Jackson.newObjectMapper();
 	}
 
-	private enum Direction {
-		SERIALISE, DESERIALISE, ALL;
+	protected Validator validator() {
+		return Validation.buildDefaultValidatorFactory().getValidator();
 	}
 
-	protected final class Fixture {
+	protected static final class Fixture<T> {
+
 		private final String fixture;
 		private final T representation;
-		private Direction direction = Direction.ALL;
-		private Optional<Boolean> validation = Optional.empty();
+		private SerialisationMode serialisationMode = SerialisationMode.ALL;
+		private ValidationMode validationMode = ValidationMode.NONE;
 
-		public Fixture(String fixture, T representation) {
-			this.fixture = fixture;
-			this.representation = representation;
+		public static <T> Fixture<T> from(String fixture, T representation) {
+			return new Fixture<>(fixture, representation);
 		}
 
-		public Fixture(String fixture, T representation, Direction direction, Optional<Boolean> validation) {
+		private Fixture(String fixture, T representation) {
 			this.fixture = fixture;
 			this.representation = representation;
-			this.direction = direction;
-			this.validation = validation;
 		}
 
 		public String getFixture() {
@@ -124,32 +162,48 @@ public abstract class RepresentationTest<T> {
 			return representation;
 		}
 
-		public Direction getDirection() {
-			return direction;
+		public SerialisationMode getSerialisationMode() {
+			return serialisationMode;
 		}
 
-		public Optional<Boolean> getValidation() {
-			return validation;
+		public ValidationMode getValidationMode() {
+			return validationMode;
 		}
 
-		public Fixture serialiseOnly() {
-			direction = Direction.SERIALISE;
+		public Fixture<T> serialise() {
+			return serialisationMode(SerialisationMode.SERIALISE);
+		}
+
+		public Fixture<T> deserialise() {
+			return serialisationMode(SerialisationMode.DESERIALISE);
+		}
+
+		public Fixture<T> serialisationMode(SerialisationMode serialisationMode) {
+			this.serialisationMode = serialisationMode;
 			return this;
 		}
 
-		public Fixture deserialiseOnly() {
-			direction = Direction.DESERIALISE;
-			return this;
+		public Fixture<T> validate() {
+			return validationMode(ValidationMode.VALIDATE);
 		}
 
-		public Fixture validate() {
-			validation = Optional.of(true);
-			return this;
+		public Fixture<T> invalidate() {
+			return validationMode(ValidationMode.INVALIDATE);
 		}
 
-		public Fixture invalidate() {
-			validation = Optional.of(false);
+		public Fixture<T> validationMode(ValidationMode validationMode) {
+			this.validationMode = validationMode;
 			return this;
 		}
+	}
+
+
+	private enum SerialisationMode {
+		SERIALISE, DESERIALISE, ALL
+	}
+
+
+	private enum ValidationMode {
+		NONE, VALIDATE, INVALIDATE
 	}
 }
