de807e138281c8fa219d2b20ebffd98aadf4e291
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2016-04-09 18:39:41 +1000

    Created common Jdk8Bundle

---
+++ core/src/main/java/me/stockwells/util/core/Jdk8Bundle.java
@@ -0,0 +1,18 @@
+package me.stockwells.util.core;
+
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import io.dropwizard.Bundle;
+import io.dropwizard.java8.Java8Bundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+
+public class Jdk8Bundle implements Bundle {
+	@Override
+	public void initialize(Bootstrap<?> bootstrap) {
+		bootstrap.getObjectMapper().registerModule(new ParameterNamesModule());
+		bootstrap.addBundle(new Java8Bundle());
+	}
+
+	public void run(Environment environment) {}
+}
