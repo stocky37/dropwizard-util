6e2b71190660df146f7540fc305d5ad2cb46c1bc
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2016-04-15 11:03:58 +1000

    Implemented some better async handling for loading cache

--- cache/src/main/java/me/stockwells/util/cache/StandardLoadingCache.java
+++ cache/src/main/java/me/stockwells/util/cache/StandardLoadingCache.java
@@ -3,6 +3,9 @@ package me.stockwells.util.cache;
 import com.google.common.cache.AbstractLoadingCache;
 import com.google.common.cache.CacheLoader;
 import com.google.common.util.concurrent.ExecutionError;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.google.common.util.concurrent.Uninterruptibles;
 
@@ -12,10 +15,15 @@ import java.io.Serializable;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ParametersAreNonnullByDefault
 public abstract class StandardLoadingCache<K extends Serializable, V extends Serializable>
 	extends AbstractLoadingCache<K, V> {
+
+	static final Logger logger = Logger.getLogger(StandardLoadingCache.class.getName());
 
 	private final CacheLoader<K, V> loader;
 
@@ -49,10 +57,34 @@ public abstract class StandardLoadingCache<K extends Serializable, V extends Ser
 
 	@Override
 	public void refresh(K key) {
+		final ListenableFuture<V> loadingFuture = loadFuture(key);
+		loadingFuture.addListener(() -> {
+			try {
+				put(key, getFromFuture(key, loadingFuture));
+			} catch(Throwable t) {
+				logger.log(Level.WARNING, "Exception thrown during refresh", t);
+			}
+		}, MoreExecutors.directExecutor());
+	}
+
+	private V getFromFuture(K key, Future<V> newValue) throws ExecutionException {
+		return Optional.ofNullable(Uninterruptibles.getUninterruptibly(newValue))
+			.orElseThrow(() -> new CacheLoader.InvalidCacheLoadException("CacheLoader returned null for key " + key + "."));
+	}
+
+	private ListenableFuture<V> loadFuture(K key) {
 		try {
-			put(key, Uninterruptibles.getUninterruptibly(loader.reload(key, get(key))));
-		} catch(Exception e) {
-			System.out.printf("Exception refreshing parseKey '%s': ", key);
+			final V previousValue = getIfPresent(key);
+			if(previousValue == null) {
+				return Futures.immediateFuture(loader.load(key));
+			}
+			ListenableFuture<V> newValue = loader.reload(key, previousValue);
+			return newValue == null ? Futures.immediateFuture(null) : newValue;
+		} catch(Throwable t) {
+			if(t instanceof InterruptedException) {
+				Thread.currentThread().interrupt();
+			}
+			return Futures.immediateFailedFuture(t);
 		}
 	}
 
