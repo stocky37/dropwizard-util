b521ce23c7007db84d3d7a2181580fc88867f0fb
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2016-08-18 21:09:56 +1000

    Fixed up the hibernate bundle a little

--- ogm/src/main/java/com/github/stocky37/util/ogm/OgmAbstractDAO.java
+++ ogm/src/main/java/com/github/stocky37/util/ogm/OgmAbstractDAO.java
@@ -4,6 +4,9 @@ import io.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.ogm.OgmSession;
 import org.hibernate.ogm.OgmSessionFactory;
 
+import java.io.Serializable;
+import java.util.Optional;
+
 public abstract class OgmAbstractDAO<E> extends AbstractDAO<E> {
 	public OgmAbstractDAO(OgmSessionFactory sessionFactory) {
 		super(sessionFactory);
@@ -12,5 +15,9 @@ public abstract class OgmAbstractDAO<E> extends AbstractDAO<E> {
 	@Override
 	protected OgmSession currentSession() {
 		return (OgmSession)super.currentSession();
+	}
+
+	protected Optional<E> findById(String query, Serializable id) {
+		return Optional.ofNullable(uniqueResult(namedQuery(query).setParameter("id", id)));
 	}
 }
--- ogm/src/main/java/com/github/stocky37/util/ogm/OgmHibernateBundle.java
+++ ogm/src/main/java/com/github/stocky37/util/ogm/OgmHibernateBundle.java
@@ -7,6 +7,7 @@ import com.google.common.collect.ImmutableList;
 import io.dropwizard.Configuration;
 import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.hibernate.ScanningHibernateBundle;
 import io.dropwizard.hibernate.UnitOfWorkApplicationListener;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -14,7 +15,6 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.ogm.OgmSessionFactory;
 import org.hibernate.ogm.boot.OgmSessionFactoryBuilder;
 import org.hibernate.ogm.engine.spi.OgmSessionFactoryImplementor;
-import org.hibernate.ogm.hibernatecore.impl.OgmSessionFactoryImpl;
 import org.hibernate.service.ServiceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,12 +26,16 @@ import java.util.TreeSet;
 public abstract class OgmHibernateBundle<T extends Configuration>
 	implements ConfiguredBundle<T>, NoSQLDatabaseConfiguration<T> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(OgmHibernateBundle.class);
+	private static final Logger LOG = LoggerFactory.getLogger(OgmHibernateBundle.class);
 
 	private final Collection<Class<?>> entities;
 
 	private OgmSessionFactory sessionFactory;
 	private boolean lazyLoadingEnabled = true;
+
+	protected OgmHibernateBundle(String pckg) {
+		this(ScanningHibernateBundle.findEntityClassesFromDirectory(new String[]{pckg}));
+	}
 
 	protected OgmHibernateBundle(Class<?> entity, Class<?>... entities) {
 		this(ImmutableList.<Class<?>>builder().add(entity).add(entities).build());
@@ -46,24 +50,22 @@ public abstract class OgmHibernateBundle<T extends Configuration>
 	}
 
 	@Override
-	public final void initialize(Bootstrap<?> bootstrap) {
-		bootstrap.getObjectMapper().registerModule(createHibernate5Module());
-	}
-
-	@Override
 	public void run(T configuration, Environment environment) throws Exception {
 		final ServiceRegistry registry = getServiceRegistry(configuration);
 
 		final OgmSessionFactoryWrapper wrapper = new OgmSessionFactoryWrapper(
-			(OgmSessionFactoryImplementor
-				) addAnnotatedClasses(new MetadataSources(registry))
-				.buildMetadata()
+			(OgmSessionFactoryImplementor)addAnnotatedClasses(new MetadataSources(registry)).buildMetadata()
 				.getSessionFactoryBuilder()
 				.unwrap(OgmSessionFactoryBuilder.class)
 				.build()
 		);
 		this.sessionFactory = wrapper;
 		registerUnitOfWorkListenerIfAbsent(environment).registerSessionFactory(name(), wrapper);
+	}
+
+	@Override
+	public final void initialize(Bootstrap<?> bootstrap) {
+		bootstrap.getObjectMapper().registerModule(createHibernate5Module());
 	}
 
 	public OgmSessionFactory getSessionFactory() {
@@ -91,18 +93,18 @@ public abstract class OgmHibernateBundle<T extends Configuration>
 
 	private MetadataSources addAnnotatedClasses(MetadataSources metadata) {
 		final SortedSet<String> entityClasses = new TreeSet<>();
-		for (Class<?> klass : entities) {
+		for(Class<?> klass : entities) {
 			metadata.addAnnotatedClass(klass);
 			entityClasses.add(klass.getCanonicalName());
 		}
-		LOGGER.info("Entity classes: {}", entityClasses);
+		LOG.info("Entity classes: {}", entityClasses);
 		return metadata;
 	}
 
 	private UnitOfWorkApplicationListener registerUnitOfWorkListenerIfAbsent(Environment environment) {
-		for (Object singleton : environment.jersey().getResourceConfig().getSingletons()) {
-			if (singleton instanceof UnitOfWorkApplicationListener) {
-				return (UnitOfWorkApplicationListener) singleton;
+		for(Object singleton : environment.jersey().getResourceConfig().getSingletons()) {
+			if(singleton instanceof UnitOfWorkApplicationListener) {
+				return (UnitOfWorkApplicationListener)singleton;
 			}
 		}
 		final UnitOfWorkApplicationListener listener = new UnitOfWorkApplicationListener();
